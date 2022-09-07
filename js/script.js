5bd3d46c24e5f09673e18278595af9c9a7c606c0
Modjara <85773458+Modjara@users.noreply.github.com>
2022-09-07 20:52:38 +0300

    GoCorona

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Меню бургер
+
+const iconMenu = document.querySelector('.icon-menu');
+if (iconMenu) {
+    const menuBody = document.querySelector('.menu__body');
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
